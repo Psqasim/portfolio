7ccceec1d0ef321edd9cd0ba4a79be1951514e8c
Psqasim <muhammadqasim0326@gmail.com>
2024-12-14 00:40:26 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
   images: {
     domains: ['images.unsplash.com', 'your-image-domain.com'], // Add any other domains you are using
   },
