06289f17f191caa1dfda0c78e0972a5574a3e219
Psqasim <muhammadqasim0326@gmail.com>
2025-05-05 15:18:49 +0500

    update

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,15 @@
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
 import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const studioHost = 'psqasim'
 
-export default defineCliConfig({ api: { projectId, dataset } })
+console.log('Loaded from .env:', { projectId, dataset })
+
+export default defineCliConfig({
+  api: {
+    projectId,
+    dataset
+  },
+  studioHost
+})
